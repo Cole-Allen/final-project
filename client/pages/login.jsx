daf307daf57708ba6dbfd4c7304224a4ec60765d
cole-allen <coleallen3333@gmail.com>
2021-11-04 10:26:22 -0700

    Login Issue looks good

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -7,7 +7,8 @@ export default class Login extends React.Component {
     super(props);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      error: ''
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,7 +24,16 @@ export default class Login extends React.Component {
       },
       body: JSON.stringify(this.state)
     })
-      .then(res => res.json())
+      .then(res => {
+        if (!res.ok) {
+          if (res.status === 401) {
+            this.setState({ error: 'Invalid Email/Login' });
+          } else {
+            this.setState({ error: 'An unexpected error occurred' });
+          }
+        }
+        return res.json();
+      })
       .then(result => {
         this.props.onSignIn(result);
         window.location.hash = 'home';
@@ -32,7 +42,8 @@ export default class Login extends React.Component {
 
   handleChange(e) {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
+      error: ''
     });
 
   }
@@ -42,6 +53,12 @@ export default class Login extends React.Component {
     const { user } = this.context;
     if (user) {
       return <Redirect to="#home"/>;
+    }
+    let err;
+    if (this.state.error) {
+      err = <div className="error">
+        {this.state.error}
+      </div>;
     }
     return (
       <div className="login-signup">
@@ -73,6 +90,7 @@ export default class Login extends React.Component {
             onChange={this.handleChange}
             required></input>
           </label>
+          {err}
           <button className="auth-submit" onClick={this.handleSubmit} type="submit">Log In</button>
         </form>
         <a href="#signup">Don&apos;t have an account? Sign-up here!</a>
