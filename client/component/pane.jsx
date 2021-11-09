77f76235459a90824e61d0d6b8630f92fbf6ef06
cole-allen <coleallen3333@gmail.com>
2021-11-08 16:03:12 -0800

    User can add/update weight

--- client/component/pane.jsx
+++ client/component/pane.jsx
@@ -12,12 +12,12 @@ export default class Pane extends React.Component {
       <div className="pane">
         <div className="pane-title">
           {this.props.title}
+          <div className="pane-unique">
+            {this.props.unique}
+          </div>
           <div className="icons">
-            <a className="schedule-icon" href="#schedule">
-              <i className="fas fa-calendar-alt"></i>
-            </a>
-            <a href="#settings">
-              <i className="fas fa-cog"></i>
+            <a href={this.props.settings}>
+              <i className="fas fa-cog" ></i>
             </a>
           </div>
 
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -5,9 +5,11 @@ import Redirect from '../lib/redirect';
 import Pane from '../component/pane';
 import Graph from '../component/graph';
 
+const token = decodeToken(window.localStorage.getItem('jwt'));
+
 const welcomePane = {
   title: `${new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(new Date().getDay())} ${new Date().getDate()}`,
-  body: 'Hello! This is an empty pane that will be used to quickly get to a workout!'
+  body: 'Hello! This is an empty pane that will be used to quickly get to a workout routine!'
 };
 
 const data = [
@@ -43,40 +45,158 @@ const data = [
 
 export default class Home extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: '',
+      weight: '',
+      editing: false,
+      dayId: '',
+      loading: true
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.loadData = this.loadData.bind(this);
+  }
+
   componentDidMount() {
-    // Fetch user data here
+    this.loadData();
+    this.setState({
+      loading: false
+    });
+  }
+
+  loadData() {
+    const date = new Intl.DateTimeFormat('en-US').format(new Date());
+    let dayData;
+    fetch(`/api/${token.userId}/weight`)
+      .then(res => res.json())
+      .then(data => {
+        for (const i in data) {
+          if (date === new Intl.DateTimeFormat('en-US').format(new Date(data[i].date))) {
+            dayData = data[i];
+          }
+        }
+        if (dayData) {
+          this.setState({
+            weight: dayData.weight,
+            dayId: dayData.historyId
+          });
+        } else {
+          this.setState({
+            error: 'Loading error'
+          });
+        }
+      });
+  }
+
+  handleChange(e) {
+    this.setState({
+      weight: e.target.value
+    });
+  }
+
+  handleClick(e) {
+    this.setState({
+      editing: true
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      editing: false
+    });
+    if (this.state.dayId) {
+      fetch(`/api/weight/${this.state.dayId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state)
+      })
+        .then(res => {
+          if (!res.ok) {
+            this.setState({ error: 'An unexpected error occurred' });
+          }
+          return res.json();
+        });
+
+    } else {
+      fetch(`/api/${token.userId}/weight`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state)
+      })
+        .then(res => {
+          if (!res.ok) {
+            this.setState({ error: 'An unexpected error occurred' });
+          }
+          return res.json();
+        })
+        .then(data => {
+          this.setState({
+            dayId: data.historyId
+          });
+        });
+    }
+
   }
 
   render() {
+    const weightTitleNE = this.state.weight ? `${this.state.weight} lbs` : 'Weight not set';
+
+    const weightTitle = this.state.editing
+      ? <form method="POST" className="weight-form">
+        <label className="weight-title">
+          <input className="weight-input" type="number" onChange={this.handleChange} value={this.state.weight} /> lbs
+        </label>
+        <button className="weight-button" type='submit' onClick={this.handleSubmit}>
+          <i className="far fa-plus-square"></i>
+          </button>
+    </form>
+      : <div>
+        {weightTitleNE}
+        <button className="weight-button" onClick={this.handleClick}>
+          <i className="far fa-calendar-plus"></i>
+        </button>
+      </div>;
+
+    const error = this.state.error ? <div className="error">{this.state.error}</div> : '';
+
+    if (this.state.loading) {
+      return <div>Loading</div>;
+    }
+
     if (!this.context.user) {
       return <Redirect to="#login"/>;
     }
-    const token = decodeToken(window.localStorage.getItem('jwt'));
 
     return (
       <div className="home">
         <h1 className="home-title">Home</h1>
-        <div className="home-profile-image">
-          <img src="./images/smallerprofile.png"/>
-        </div>
-        <h2 className="home-greeting">Hello, {token.firstName}!</h2>
-        <Pane title={welcomePane.title}>
-          <div className="pane-body">
-            {welcomePane.body}
+        {error}
+        <div className="home-head">
+          <div className="home-profile-image">
+            <img src="./images/smallerprofile.png" />
           </div>
-        </Pane>
+          <h2 className="home-greeting">Hello, {token.firstName}!</h2>
+          <div className="home-welcome-pane">
+            <Pane title={welcomePane.title} settings="#calender">
+              <div className="pane-body">
+                {welcomePane.body}
+              </div>
+            </Pane>
+          </div>
+        </div>
+
         <div className="widgets">
-          <Pane title="Weight" history="">
+          <Pane unique={weightTitle} title="Weight" history="" settings="#weight-history">
             <Graph data={data}/>
-          </Pane>
-          <Pane title="Weight" history="">
-            <Graph data={data} />
-          </Pane>
-          <Pane title="Weight" history="">
-            <Graph data={data} />
-          </Pane>
-          <Pane title="Weight" history="">
-            <Graph data={data} />
           </Pane>
         </div>
         <div className="logout">
